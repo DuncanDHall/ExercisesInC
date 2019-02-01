29ab94cb66ab83c5deaa20d9134454b14d936c37
Duncan Hall <duncandhall@gmail.com>
2019-02-01 00:56:39 -0500

    done with hw1

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-int other() {
-    return 1;
-}
-
 int main() {
-    other();
+    int x = 5;
+    int y = x + 1;
     printf("Hello, World!\n");
     int a = 3;
     int b = 4;
     int c = a + b;
-    printf("c is %d\n", c);
+    printf("x is %d\n", x);
     return 0;
+    // I am not sure what you're fishing for here.
 }
--- exercises/ex01/hello.s
+++ exercises/ex01/hello.s
@@ -1,20 +1,5 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version macos, 10, 14
-	.globl	_other                  ## -- Begin function other
-	.p2align	4, 0x90
-_other:                                 ## @other
-	.cfi_startproc
-## %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	movl	$1, %eax
-	popq	%rbp
-	retq
-	.cfi_endproc
-                                        ## -- End function
 	.globl	_main                   ## -- Begin function main
 	.p2align	4, 0x90
 _main:                                  ## @main
@@ -26,19 +11,18 @@ _main:                                  ## @main
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register %rbp
 	subq	$32, %rsp
-	movl	$0, -4(%rbp)
-	callq	_other
 	leaq	L_.str(%rip), %rdi
-	movl	%eax, -20(%rbp)         ## 4-byte Spill
+	movl	$0, -4(%rbp)
+	movl	$5, -8(%rbp)
 	movb	$0, %al
 	callq	_printf
 	leaq	L_.str.1(%rip), %rdi
-	movl	$3, -8(%rbp)
-	movl	$4, -12(%rbp)
-	movl	-8(%rbp), %ecx
-	addl	-12(%rbp), %ecx
-	movl	%ecx, -16(%rbp)
-	movl	-16(%rbp), %esi
+	movl	$3, -12(%rbp)
+	movl	$4, -16(%rbp)
+	movl	-12(%rbp), %ecx
+	addl	-16(%rbp), %ecx
+	movl	%ecx, -20(%rbp)
+	movl	-8(%rbp), %esi
 	movl	%eax, -24(%rbp)         ## 4-byte Spill
 	movb	$0, %al
 	callq	_printf
@@ -55,7 +39,7 @@ L_.str:                                 ## @.str
 	.asciz	"Hello, World!\n"
 
 L_.str.1:                               ## @.str.1
-	.asciz	"c is %d\n"
+	.asciz	"x is %d\n"
 
 
 .subsections_via_symbols
