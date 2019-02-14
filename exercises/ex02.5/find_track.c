a9068c2f7ffed5a58c92d572ee1fd6193c22d997
Duncan Hall <duncandhall@gmail.com>
2019-02-13 20:40:47 -0500

    finished find track

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -38,7 +38,24 @@ void find_track(char search_for[])
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+    regex_t regex;
+    int status;
+    char search_for[80];
+
+    status = regcomp(&regex, pattern, 0);
+    if (status) {
+        puts("Could not compile pattern");
+        exit(1);
+    }
+
+    int i;
+    for (i=0; i<NUM_TRACKS; i++) {
+        status = regexec(&regex, tracks[i], 0, NULL, 0);
+        if (!status) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
+    regfree(&regex);
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -59,7 +76,7 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
+    find_track_regex(search_for);
     //find_track_regex(search_for);
 
     return 0;
