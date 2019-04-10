c9f6e931adacf361915d6b11b621117bd306cc15
Duncan Hall <duncandhall@gmail.com>
2019-04-09 23:25:50 -0400

    done with HW 08

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 #include <glib.h>
+#include <glib/gstdio.h>
 
+int CCC = 0;
 
 int get_next_word(FILE *fp, char **word_ptr)
 {
@@ -18,32 +21,38 @@ int get_next_word(FILE *fp, char **word_ptr)
         c = fgetc(fp);
         i++;
     }
-    *word[i] = '\0';
-    *word_ptr = g_strdup(word);
+    word[i] = '\0';
+
+    *word_ptr = (char *) g_strdup(word);
+    CCC++;
     return 0;
 }
 
 void word_count(FILE *fp, GHashTable *words_hash)
 {
-    char **word_ptr;
+    char *word = NULL;
+    char **word_ptr = &word;
+    gpointer r;
     int n;
+    int *n_ptr = &n;
 
     while (!get_next_word(fp, word_ptr)) {
-        n = g_hash_table_lookup(words_hash, *word_ptr);
-        if (n == NULL) n = 0;
-        g_hash_table_replace(words_hash, *word_ptr, n+1);
+        r = g_hash_table_lookup(words_hash, *word_ptr);
+        *n_ptr = (r == NULL) ? 1 : 1 + *(int *)r;
+        g_hash_table_replace(words_hash, *word_ptr, g_memdup(n_ptr, 8));
     }
 }
 
-void print_entry(gpointer key, gpointer value, gpointer user_data)
+void print_entry(gpointer key, gpointer value, gpointer _)
 {
-    
+    g_print("%s: %d\n", (char *)key, *(int *) value);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    FILE *fp = g_fopen(argv[1]);
+    FILE *fp = g_fopen(argv[1], "r");
     GHashTable *words_hash = g_hash_table_new(g_str_hash, g_str_equal);
 
     word_count(fp, words_hash);
+    g_hash_table_foreach(words_hash, print_entry, NULL);
 }
